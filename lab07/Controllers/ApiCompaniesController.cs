36d277116832d410634df0ff142e954725a47675
jakub.piechowicz <jakub.piechowicz@microsoft.wsei.edu.pl>
2025-11-25 18:19:16 +0100

    LAB7 END

---
+++ lab07/Controllers/ApiCompaniesController.cs
@@ -0,0 +1,18 @@
+﻿using lab07.Models.Movies;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab07.Controllers;
+
+[ApiController]
+public class ApiCompaniesController(MoviesContext context): ControllerBase
+{
+
+    [HttpGet("api/companies")]
+    public List<ProductionCompany> GetCompanies(string filter)
+    {
+        return context
+            .ProductionCompanies
+            .Where(c => c.CompanyName.ToLower().Contains(filter.ToLower()))
+            .ToList();
+    }
+}
